c77574461ee6e681b693207896b83bf0fd2e70b8
Timothe <timothe.champieux@ynov.com>
2024-10-25 12:27:18 +0200

    fonctionementjeu

--- main.go
+++ main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"fmt"
+	"hangmanWeb/game/affichage"
 	recupmot "hangmanWeb/game/recupMot"
+	"math/rand"
 	"net/http"
 	"os"
 	"text/template"
 )
 
 var (
-	gameStarted bool = false
+	gameStarted  bool = false
+	essaie       int  = 8
+	reussitte    int
+	motCacher    string
+	motMasque    []string
+	motAleatoire string
 )
 
 type user struct {
@@ -66,18 +73,69 @@ func main() {
 	})
 	//------------------------------------------------------- page de jeu  ----------------------------------------------
 	type data struct {
-		User2 user
-		Mot   string
+		User2     user
+		Mot       string
+		MotCacher string
+		Essaie    int // nombre de vie
+		Reussitte int // nombre de lettre a trouver
+
 	}
+	motAleatoire = recupmot.Recup("./game/recupMot/mot.txt")
 	http.HandleFunc("/game", func(w http.ResponseWriter, r *http.Request) {
 
-		motAleatoire := recupmot.Recup("./game/recupMot/mot.txt")
-		data := data{user1, motAleatoire}
-		tmpl.ExecuteTemplate(w, "game", data)
+		motMasque = affichage.Debut(motAleatoire)
 
+		for i := 0; i < len(motMasque); i++ {
+			motMasque[i] = "_ "
+		}
+		if user1.Difficulty == "difficile" {
+			reussitte = len(motMasque) - 1
+			motMasque[0] = string(motAleatoire[0])
+		} else {
+			reussitte = len(motMasque) - 2
+			motMasque[0] = string(motAleatoire[0])
+			max := len(motMasque)
+			indexLettreAletoire := rand.Intn(max - 1)
+			motMasque[indexLettreAletoire+1] = string(motAleatoire[indexLettreAletoire+1])
+		}
+		for i := 0; i < len(motMasque); i++ {
+			motCacher += motMasque[i]
+		}
+
+		data := data{user1, motAleatoire, motCacher, essaie, reussitte}
+
+		tmpl.ExecuteTemplate(w, "game", data)
 	})
 
-	//------------------------------------------------------- page de jeu  ----------------------------------------------
+	//-------------------------------------------------------  traitement page de jeu  ----------------------------------------------
+
+	http.HandleFunc("/game/treatment", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Redirect(w, r, "/game", http.StatusSeeOther)
+			return
+		}
+
+		lettre := r.FormValue("lettre")
+		if len(lettre) == 1 {
+			var count int
+			for i := 0; i < len(motAleatoire); i++ {
+				if lettre == string(motAleatoire[i]) && lettre != motMasque[i] {
+					motMasque[i] = lettre
+					reussitte--
+					count++
+				}
+			}
+			if count == 0 {
+				essaie--
+				fmt.Printf("\n\033[31mLa lettre que vous avez entré n'est pas contenue dans le mot.\033[0m \n")
+			}
+		}
+		for i := 0; i < len(motMasque); i++ {
+			motCacher += motMasque[i]
+		}
+		http.Redirect(w, r, "/game", http.StatusSeeOther)
+	})
+	//-------------------------------------------------------  traitement page de jeu  ----------------------------------------------
 	fmt.Println("Serveur démarré sur http://localhost:8080/lancement")
 	http.ListenAndServe(":8080", nil)
 }
